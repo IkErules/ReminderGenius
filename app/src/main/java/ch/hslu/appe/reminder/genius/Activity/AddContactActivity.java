1f7ed39bb506fa0f0a9d59bfc75821e60abef600
Roman-Schraner <roman.schraner@stud.hslu.ch>
2019-05-01 19:56:36 +0200

    RE-2: Basic Contact functionality (Add, Edit, DeleteAll) implemented.

--- app/src/main/java/ch/hslu/appe/reminder/genius/Activity/AddContactActivity.java
+++ app/src/main/java/ch/hslu/appe/reminder/genius/Activity/AddContactActivity.java
@@ -25,6 +25,7 @@ public class AddContactActivity extends AppCompatActivity {
     public static final int PICK_CONTACT_REQUEST = 59;
     public static final String EDIT_CONTACT = "contact.to.edit";
     private ContactViewModel contactViewModel;
+    private Contact contact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,16 +35,17 @@ public class AddContactActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         Intent caller = getIntent();
-        Contact contact = caller.getParcelableExtra(EDIT_CONTACT);
-        // TODO: Process Contact and Display
+        if (caller.hasExtra(EDIT_CONTACT)) {
+            this.contact = caller.getParcelableExtra(EDIT_CONTACT);
+            Log.d("AddContactActivity", "Started from intent, loading contact: " + this.contact.toString());
+            this.setTextFieldsFromContact();
+        }
 
         this.contactViewModel = ViewModelProviders.of(this).get(ContactViewModel.class);
 
         addSearchListener();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-
     }
 
     @Override
@@ -62,9 +64,9 @@ public class AddContactActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_save) {
-            Contact contact = createContactFromTextFields();
-            Log.i("AddContactActivity", "Adding Contact to DB, contact: " + contact.toString());
-            this.contactViewModel.insert(contact);
+            this.createContactFromTextFields();
+            Log.i("AddContactActivity", "Adding Contact to DB, contact: " + this.contact.toString());
+            this.contactViewModel.insert(this.contact);
             this.finish();
         }
 
@@ -129,19 +131,61 @@ public class AddContactActivity extends AppCompatActivity {
         phone.setText(searchContact.getPhone());
     }
 
-    private Contact createContactFromTextFields() {
+    private void setTextFieldsFromContact() {
+        TextView firstname = findViewById(R.id.add_contact_firstname_text_view);
+        firstname.setText(this.contact.getFirstName());
+
+        TextView name = findViewById(R.id.add_contact_name_text_view);
+        name.setText(this.contact.getLastName());
+
+        TextView address = findViewById(R.id.add_contact_address_text_view);
+        address.setText(this.contact.getStreet());
+
+        TextView zip = findViewById(R.id.add_contact_zip_text_view);
+        zip.setText(Integer.toString(this.contact.getZip()));
+
+        TextView city = findViewById(R.id.add_contact_city_text_view);
+        city.setText(this.contact.getCity());
+
+        TextView canton = findViewById(R.id.add_contact_canton_text_view);
+        canton.setText(this.contact.getCanton());
+
+        TextView phone = findViewById(R.id.add_contact_phone_text_view);
+        phone.setText(this.contact.getPhone());
+
+        TextView email = findViewById(R.id.add_contact_email_text_view);
+        email.setText(this.contact.getMail());
+    }
+
+    private void createContactFromTextFields() {
         // TODO: Cleanup Code and fill all fields!
         String firstname = ((TextView) findViewById(R.id.add_contact_firstname_text_view)).getText().toString();
         String lastname = ((TextView) findViewById(R.id.add_contact_name_text_view)).getText().toString();
         String street = ((TextView) findViewById(R.id.add_contact_address_text_view)).getText().toString();
-        int zip = Integer.parseInt(((TextView) findViewById(R.id.add_contact_zip_text_view)).getText().toString());
+        int zip = 0;
+        try {
+            zip = Integer.parseInt(((TextView) findViewById(R.id.add_contact_zip_text_view)).getText().toString());
+        } catch (Exception ex) {
+            Log.e("AddContactActivity", "No ZIP entered!");
+        }
         String city = ((TextView) findViewById(R.id.add_contact_city_text_view)).getText().toString();
         String canton = ((TextView) findViewById(R.id.add_contact_canton_text_view)).getText().toString();
         String phone = ((TextView) findViewById(R.id.add_contact_phone_text_view)).getText().toString();
         String mail = ((TextView) findViewById(R.id.add_contact_email_text_view)).getText().toString();
 
-        Contact contact = new Contact(firstname, lastname, "", "", phone, mail, street, city, zip, canton, "");
-        return contact;
+        if (this.contact == null) {
+            this.contact = new Contact(firstname, lastname, "", "", phone, mail, street, city, zip, canton, "");
+        } else {
+            // Reuse existing contact in order to maintain Primary Key.
+            this.contact.setFirstName(firstname);
+            this.contact.setLastName(lastname);
+            this.contact.setPhone(phone);
+            this.contact.setMail(mail);
+            this.contact.setStreet(street);
+            this.contact.setCity(city);
+            this.contact.setZip(zip);
+            this.contact.setCanton(canton);
+        }
     }
 
     private void resetSearchView() {
