4312f9cecea4f3722531da131ae28eb2bf716d69
Roman-Schraner <roman.schraner@stud.hslu.ch>
2019-05-05 14:53:36 +0200

    Redesign ShowContact Activity

--- app/src/main/java/ch/hslu/appe/reminder/genius/Activity/ShowContactActivity.java
+++ app/src/main/java/ch/hslu/appe/reminder/genius/Activity/ShowContactActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import ch.hslu.appe.reminder.genius.DB.Entity.Contact;
@@ -30,16 +32,69 @@ public class ShowContactActivity extends AppCompatActivity {
         Log.d("ShowContactActivity", "Got Contact to display: " + this.contact.toString());
 
         this.showContact();
+
+        this.registerListeners();
     }
 
     private void showContact() {
         // TODO: Display Contact
         ((TextView) findViewById(R.id.show_contact_name_text_view)).setText(this.contact.getFirstName() + " " + this.contact.getLastName());
-        ((TextView) findViewById(R.id.show_contact_street_text_view)).setText(this.contact.getStreet());
-        ((TextView) findViewById(R.id.show_contact_zip_text_view)).setText(Integer.toString(this.contact.getZip()));
-        ((TextView) findViewById(R.id.show_contact_city_text_view)).setText(this.contact.getCity());
+        ((TextView) findViewById(R.id.show_contact_address_text_view)).setText(
+                        this.contact.getStreet() + "\n" +
+                        Integer.toString(this.contact.getZip()) + " " + this.contact.getCity() + "\n" +
+                        this.contact.getCanton() + " " + this.contact.getCountry()
+        );
         ((TextView) findViewById(R.id.show_contact_mail_text_view)).setText(this.contact.getMail());
         ((TextView) findViewById(R.id.show_contact_phone_text_view)).setText(this.contact.getPhone());
+    }
 
+    private void registerListeners() {
+        TextView address = (TextView) findViewById(R.id.show_contact_address_text_view);
+        address.setOnClickListener((View v) -> {
+            onContactAddressClicked();
+        });
+
+        TextView mail = (TextView) findViewById(R.id.show_contact_mail_text_view);
+        mail.setOnClickListener((View v) -> {
+            onContactMailClicked();
+        });
+        TextView phone = (TextView) findViewById(R.id.show_contact_phone_text_view);
+        phone.setOnClickListener((View v) -> {
+            onContactPhoneClicked();
+        });
+    }
+
+    private void onContactAddressClicked() {
+        // Create Google Maps intent: https://developer.android.com/guide/components/intents-common#Maps
+        Uri geoLocation = Uri.parse("geo:0,0?q=" +
+                Uri.encode(this.contact.getStreet()) + ", " +
+                Uri.encode(Integer.toString(this.contact.getZip())) + " " +
+                Uri.encode(this.contact.getCity()) +  ", " +
+                Uri.encode(this.contact.getCanton())
+        );
+        Log.d("ShowContactActivity", "Parsed URI for Maps: " +  geoLocation.toString());
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    private void onContactMailClicked() {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, this.contact.getMail());
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    private void onContactPhoneClicked() {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + this.contact.getPhone()));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
+
