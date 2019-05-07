9e9390b75cf5a72f932dd342dabb4b0b1c509d35
Schraner Roman iaschran <roman.schraner@stud.hslu.ch>
2019-05-08 01:56:22 +0200

    Basic Image Viewing Option (may needs refactoring to horizontal-scrollable-imageview).

--- app/src/main/java/ch/hslu/appe/reminder/genius/Activity/ShowInstallationActivity.java
+++ app/src/main/java/ch/hslu/appe/reminder/genius/Activity/ShowInstallationActivity.java
@@ -1,21 +1,28 @@
 package ch.hslu.appe.reminder.genius.Activity;
 
+import androidx.annotation.DrawableRes;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
+import androidx.viewpager2.widget.ViewPager2;
+
+
+import java.util.ArrayList;
 import java.util.List;
 
-import ch.hslu.appe.reminder.genius.Adapter.ContactListAdapter;
+import ch.hslu.appe.reminder.genius.Adapter.ShowInstallationViewPagerAdapter;
 import ch.hslu.appe.reminder.genius.DB.Entity.Contact;
 import ch.hslu.appe.reminder.genius.DB.Entity.Installation;
 import ch.hslu.appe.reminder.genius.DB.Entity.ProductCategory;
@@ -31,6 +38,8 @@ public class ShowInstallationActivity extends AppCompatActivity {
     private Installation installation;
     private Contact contact;
     private ProductCategory productCategory;
+
+    private ViewPager2 installationImageViewPager;
 
     private ContactViewModel contactViewModel;
     private ProductCategoryViewModel productCategoryViewModel;
@@ -48,6 +57,16 @@ public class ShowInstallationActivity extends AppCompatActivity {
         this.contactViewModel = ViewModelProviders.of(this).get(ContactViewModel.class);
         this.productCategoryViewModel = ViewModelProviders.of(this).get(ProductCategoryViewModel.class);
         this.installationViewModel = ViewModelProviders.of(this).get(InstallationViewModel.class);
+
+        this.installationImageViewPager = findViewById(R.id.show_installation_image_view_pager);
+
+        List<Drawable> list = new ArrayList<>();
+        list.add(getDrawable(R.drawable.eagle));
+        list.add(getDrawable(R.drawable.bear));
+        list.add(getDrawable(R.drawable.bonobo));
+        list.add(getDrawable(R.drawable.horse));
+
+        this.installationImageViewPager.setAdapter(new ShowInstallationViewPagerAdapter(this, list, this.installationImageViewPager));
 
         Intent caller = getIntent();
         this.installation = caller.getParcelableExtra(SHOW_INSTALLATION);
@@ -99,6 +118,12 @@ public class ShowInstallationActivity extends AppCompatActivity {
         ((TextView) findViewById(R.id.show_installation_name_text_view)).setText("Installation " + this.installation.getInstallationId());
         ((TextView) findViewById(R.id.show_installation_notes_text_view)).setText(this.installation.getNotes());
         ((TextView) findViewById(R.id.show_installation_expiredate_text_view)).setText("Nächster Service: " + this.installation.getExpireDate().toString());
+
+        ((CheckBox) findViewById(R.id.show_installation_notify_customer_mail_check_box)).setActivated(this.installation.getNotifyCustomerMail());
+        ((CheckBox) findViewById(R.id.show_installation_notify_customer_sms_check_box)).setActivated(this.installation.getNotifyCustomerSms());
+        ((CheckBox) findViewById(R.id.show_installation_notify_technician_mail_check_box)).setActivated(this.installation.getNotifyCreatorMail());
+        ((CheckBox) findViewById(R.id.show_installation_notify_technician_sms_check_box)).setActivated(this.installation.getNotifyCreatorSms());
+
     }
 
     /*
